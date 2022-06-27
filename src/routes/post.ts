acbcf990d0c9b317519ce3e9c97da2b01287d3dd
Mohammed <mohammednady128@gmail.com>
2022-06-27 14:26:39 +0200

    post:crud#2 added update and get posts

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,21 +1,39 @@
 import { RequestHandler, Router } from "express";
 import { resizeImage, uploader,prisma, StatusCodes } from "../utils";
-import { authMiddleware } from "../middleware/authMiddleware";
 import { unlinkSync } from "fs";
-import { BadRequestError, NotAuthenticatedError, NotFoundError } from "../errors";
+import { BadRequestError, ForbiddenError, NotFoundError } from "../errors";
 
 export const postRouter = Router()
 
 const feed:RequestHandler = async(req,res)=>{}
 
-const getPosts: RequestHandler = async (req, res) => {};
+const getPosts: RequestHandler = async (req, res) => {
+    const {id,stDate,enDate} = req.query
+    let posts;
+    if(!id){
+        throw new BadRequestError("Please Provide User id")
+    }
+    let queryObj:Record<string,any> = {
+        id:(id==="self")?req.user?.id:parseInt(id as string),
+        private:(id==="self")?{in:[true,false]}:false,
+        createTime:{
+            gte:(stDate)?new Date(stDate as string).toISOString():new Date(0).toISOString(),
+            lte:(enDate)?new Date(enDate as string).toISOString():new Date().toISOString()
+        }
+    }
+    posts = await prisma.post.findMany({where:queryObj,include:{images:true}})
+    return res.json({posts})
+};
 
 const getPost:RequestHandler = async(req,res)=>{
     const id = parseInt(req.params.id)
     const post = await prisma.post.findUnique({where:{id}
-    ,select:{body:true,images:true}})
+    ,include:{images:true}})
     if(!post){
         throw new NotFoundError("Post Not Found")
+    }
+    if(post.authorId!==req.user?.id && post.private===true){
+        throw new ForbiddenError("You Cant View This Post")
     }
     return res.status(StatusCodes.OK).json({post});
 }
@@ -25,7 +43,7 @@ const createPost:RequestHandler = async(req,res)=>{
     if(!req.files && !body){
         throw new BadRequestError("Cant Create Empty Post")
     }
-    const post = await prisma.post.create({data:{body,authorId:req.user?.id}})
+    const post = await prisma.post.create({data:{body,authorId:req.user!.id}})
     if(req.files){
         (req.files as Array<Express.Multer.File>).forEach(async(file,idx)=>{
             await resizeImage(file.path,file.filename,file.destination)
@@ -36,17 +54,28 @@ const createPost:RequestHandler = async(req,res)=>{
     return res.status(StatusCodes.CREATED).json({msg:"Post Created",post})
 }
     
+const updatePost: RequestHandler = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const old = await prisma.post.findUnique({where:{id}})
+    if(!old){
+        throw new NotFoundError("Post Not Found")
+    }
+    if(old.authorId!==req.user?.id){
+        throw new ForbiddenError("You Cant Update This Post")
+    }
+    const post = await prisma.post.update({where:{id},data:{body:req.body.body}})
+    return res.json({post})
+};
 
-const updatePost: RequestHandler = async (req, res) => {};
 
 const deletePost:RequestHandler = async(req,res)=>{
     const id = parseInt(req.params.id);
-    const post = await prisma.post.findFirst({where:{id},include:{images:true}})
+    const post = await prisma.post.findUnique({where:{id},include:{images:true}})
     if(!post){
         throw new NotFoundError("Post Not Found")
     }
     if(post.authorId!==req.user?.id){
-        throw new NotAuthenticatedError("You Cant Delete This Post")
+        throw new ForbiddenError("You Cant Delete This Post")
     }
     await prisma.post.delete({where:{id}})
     post.images.forEach((img)=>{
@@ -55,6 +84,10 @@ const deletePost:RequestHandler = async(req,res)=>{
     return res.status(StatusCodes.OK).json({post})
 }
 
-postRouter.get("/:id",authMiddleware,getPost)
-postRouter.post("/create",authMiddleware,uploader.array("photos"),createPost);
-postRouter.delete("/delete/:id",authMiddleware,deletePost)
+
+// post
+postRouter.get("/",getPosts)
+postRouter.get("/:id",getPost)
+postRouter.post("/create",uploader.array("images"),createPost);
+postRouter.patch("/update/:id",updatePost)
+postRouter.delete("/delete/:id",deletePost)
