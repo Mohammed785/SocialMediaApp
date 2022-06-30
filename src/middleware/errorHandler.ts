7c55bfbb1db97033f118b791e9e6925e15877126
Mohammed <mohammednady128@gmail.com>
2022-06-30 17:33:46 +0200

    group request routes

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -16,6 +16,7 @@ export const errorHandler: ErrorRequestHandler = async (err, req, res, next) => 
         CustomError.code = err.code
         if(err.code==="P2025"){
             CustomError.message = "Record to update not found.";
+            CustomError.statusCode = 400;
         }else if(err.code==="P2021"){
             CustomError.message = (process.env.NODE_ENV==="dev")
             ?"The table `StatusView` does not exist in the current database"
@@ -24,11 +25,11 @@ export const errorHandler: ErrorRequestHandler = async (err, req, res, next) => 
             CustomError.message = "Server has closed the connection.";
         }else if(err.code==="P2002"){
             CustomError.message = "Value Is Not Unique.";
-            CustomError.statusCode = 400
+            CustomError.statusCode = 500
         }
         else if(err.code==="P2013"){
             CustomError.message = "Required Values Are Missing.";
-            CustomError.statusCode = 400;
+            CustomError.statusCode = 500;
         }else if(err.code==="P2003"){
             const name = err.meta!.field_name as string
             CustomError.message = `${name.slice(0,-2)} Not Found`;
--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,14 +1,14 @@
 import { RequestHandler, Router } from "express";
 import { unlinkSync } from "fs";
 import { BadRequestError, ForbiddenError, NotFoundError } from "../errors";
-import { prisma, resizeImage, StatusCodes, uploader } from "../utils";
+import { prisma, resizeImage, StatusCodes, uploader, userSelect } from "../utils";
 
 export const groupRouter = Router()
 
 
 const getGroup:RequestHandler = async(req,res)=>{
     const id = parseInt(req.params.id)
-    const group = await prisma.group.findUnique({where:{id}})
+    const group = await prisma.group.findUnique({where:{id},include:{creator:{select:userSelect}}})
     if(!group){
         throw new NotFoundError("Group Not Found")
     }
@@ -70,7 +70,112 @@ const deleteGroup:RequestHandler = async(req,res)=>{
 }
 
 
+const getGroupRequests: RequestHandler = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const group = await prisma.group.findUnique({ where: { id } });
+    if (!group) {
+        throw new NotFoundError("Group Not Found");
+    }
+    if (group.creatorId !== req.user?.id) {
+        throw new ForbiddenError("You Can't View This Group Requests");
+    }
+    const requests = await prisma.groupRequest.findMany({
+        where: { groupId: id, accepted: null },
+        include: { sender: { select: userSelect } },
+    });
+    return res.json({ requests });
+};
+
+const acceptGroupRequest: RequestHandler = async (req, res) => {
+    const groupId = parseInt(req.params.groupId),
+        userId = parseInt(req.params.userId);
+    const group = await prisma.group.findUnique({ where: { id:groupId } });
+    if (!group) {
+        throw new NotFoundError("Group Not Found");
+    }
+    if (group.creatorId !== req.user?.id) {
+        throw new ForbiddenError("You Can't Respond To This Group Requests");
+    }
+    let request = await prisma.groupRequest.findUnique({where:{groupId_senderId:{
+        groupId,senderId:userId
+    }}})
+    if(request?.accepted===true){
+        return res.json({msg:"User Already A Member"})
+    }
+    request = await prisma.groupRequest.update({
+        where: {
+            groupId_senderId: {
+                groupId,
+                senderId: userId,
+            },
+        },
+        data: { accepted: true, acceptTime: new Date() },
+    });
+    await prisma.groupMembership.create({data:{groupId,userId}})
+    return res.json({ msg: "Accepted", request });
+    
+};
+
+
+const declineGroupRequest: RequestHandler = async (req, res) => {
+    const groupId = parseInt(req.params.groupId),
+        userId = parseInt(req.params.userId);
+    const group = await prisma.group.findUnique({ where: { id: groupId } });
+    if (!group) {
+        throw new NotFoundError("Group Not Found");
+    }
+    if (group.creatorId !== req.user?.id) {
+        throw new ForbiddenError("You Can't Respond To This Group Requests");
+    }
+    const request = await prisma.groupRequest.delete({
+        where: {
+            groupId_senderId: {
+                groupId,
+                senderId: userId,
+        }}
+    });
+    return res.json({ msg: "Request Declined", request });
+};
+
+const cancelGroupRequest: RequestHandler = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const request = await prisma.groupRequest.findUnique({
+        where: {
+            groupId_senderId: {
+                groupId: id,
+                senderId: req.user!.id,
+        }}
+    });
+    if (!request) {
+        throw new NotFoundError("No Request Found To This Group");
+    }
+    if (request.senderId !== req.user?.id) {
+        throw new ForbiddenError("You Can't Cancel This Request");
+    }
+    if (request.accepted === true) {
+        throw new BadRequestError(
+            "Request Already Accepted Try To Leave The The Group"
+        );
+    }
+    await prisma.groupRequest.delete({
+        where: {
+            groupId_senderId: {
+                groupId: id,
+                senderId: req.user!.id,
+        }}
+    });
+    return res.json({ request });
+};
+
+
 groupRouter.post("/create",uploader.single("image"),createGroup)
 groupRouter.patch("/update/:id",uploader.single("image"),editGroup)
 groupRouter.delete("/delete/:id",deleteGroup)
 groupRouter.get("/:id",getGroup)
+
+
+groupRouter.get("/:id/request/all", getGroupRequests);
+groupRouter.patch("/:groupId/request/:userId/accept", acceptGroupRequest);
+groupRouter.delete("/:groupId/request/:userId/decline",declineGroupRequest)
+groupRouter.delete("/:id/request/cancel",cancelGroupRequest)
+
