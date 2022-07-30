b3abaea5026b68f0de67bc366849ff960a473366
Mohammed <mohammednady128@gmail.com>
2022-07-30 17:06:09 +0200

    create feed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css"
-import './index.css';
 import "@popperjs/core"
 import "bootstrap/dist/js/bootstrap.min.js"
 import App from './App';
